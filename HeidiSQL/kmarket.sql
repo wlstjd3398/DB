01c1b2017ccfdfe09fd338327c90c4f907a3d0c4
wlstjd3398 <https://github.com/wlstjd3398/DB.git>
2021-07-22 12:27:14 +0900

    2021/07/22

--- HeidiSQL/kmarket.sql
+++ HeidiSQL/kmarket.sql
@@ -207,3 +207,15 @@ INSERT INTO `km_cate2` (`code1`, `code2`, `title`) VALUES (9, 5, '상품권');
 
 SELECT * FROM `km_cate2` WHERE `code`=6 ORDER BY `seq`;
 
+#product 데이터 철학샘한테 받고 km_cate1 km_cate2 조인해야함
+SELECT c2.code1, c2.code2, c1.title AS tit1, c2.title AS tit2
+FROM `km_cate1` AS c1
+JOIN `km_cate2` AS c2
+ON c1.code1 = c2.code1
+ORDER BY c2.code1, c2.code2;
+
+#히트상품
+SELECT * FROM `km_product`
+ORDER by `hit` DESC
+LIMIT 8;
+
